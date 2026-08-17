f1ef3e32ebe3ebf4d1ff97b63012372ba4835eff
yak <paredesyak@gmail.com>
2026-08-17 02:11:33 -0500

    config cords apenapigcongif

--- src/main/java/gym/ada/api/config/OpenApiConfig.java
+++ src/main/java/gym/ada/api/config/OpenApiConfig.java
@@ -1,9 +1,16 @@
 package gym.ada.api.config;
 
+import java.util.List;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @OpenAPIDefinition
 @SecurityScheme(
     name = "Bearer Authentication",
@@ -12,4 +19,13 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
     scheme = "bearer"
 )
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                    new Server()
+                        .url("https://api-rest-gyselmood-production.up.railway.app")
+                ));
+    }
 }
