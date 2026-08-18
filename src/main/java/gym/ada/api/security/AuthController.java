a4742621382b3b0f94172007b2bef888b1a8ef2c
yak <paredesyak@gmail.com>
2026-08-18 00:10:40 -0500

    fix duplicate auth mappings

--- src/main/java/gym/ada/api/security/AuthController.java
+++ src/main/java/gym/ada/api/security/AuthController.java
@@ -53,17 +53,5 @@ public class AuthController {
         );
     }
     
-    @PostMapping("/login-debug")
-    public ResponseEntity<?> loginDebug(
-            @RequestBody LoginUsuarioDto dto) {
-
-        System.out.println("========== LOGIN DEBUG ==========");
-        System.out.println("EMAIL: " + dto.getEmail());
-        System.out.println("PASSWORD RECIBIDA: " + dto.getPassword());
-        System.out.println("=================================");
-
-        return ResponseEntity.ok(
-                "CONTROLLER_OK"
-        );
-    }
+    
 }
