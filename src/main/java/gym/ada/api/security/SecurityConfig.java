bff0722ca83d350d5782c49500d0503c474aa33e
yak <paredesyak@gmail.com>
2026-08-17 23:42:38 -0500

    test: disable jwt filter temporarily

--- src/main/java/gym/ada/api/security/SecurityConfig.java
+++ src/main/java/gym/ada/api/security/SecurityConfig.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,7 +14,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
@@ -95,12 +94,12 @@ public class SecurityConfig {
             			).permitAll()
 
                 .anyRequest().authenticated()
-            )
+            );
 
-            .addFilterBefore(
+            /*.addFilterBefore(
                 jwtFilter,
                 UsernamePasswordAuthenticationFilter.class
-            );
+            );*/
 
         return http.build();
     }
